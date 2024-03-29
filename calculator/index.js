31c07871b410a94a87319e64c4aaf28740354a8f
Nauman Khalid <naumankhalid217@gmail.com>
2024-03-29 22:29:35 +0500

    CLI Calculator

---
+++ calculator/index.js
@@ -0,0 +1,52 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+//Welcome Message
+console.log("\n\tWelcome To \'CodeWithNauman\' - CLI Simple Calculator\n");
+let seperator = "_";
+// create account on npm and deploy calculator project on npm, and then install using npx command
+let answer = await inquirer.prompt([{
+        name: "firstNumber",
+        message: "Enter First Number : ",
+        type: "number"
+    },
+    {
+        name: "secondNumber",
+        message: "Enter Second Number : ",
+        type: "number"
+    },
+    {
+        type: "list",
+        name: "operator",
+        message: "Enter the Operator",
+        choices: ["Add", "Subtract", "Multiple", "Divide"]
+    }]);
+let result;
+switch (answer.operator) {
+    case "Add":
+        result = answer.firstNumber + answer.secondNumber;
+        console.log(seperator.repeat(25));
+        console.log("    Answer is : " + result);
+        console.log(seperator.repeat(25));
+        break;
+    //Subtract
+    case "Subtract":
+        result = answer.firstNumber - answer.secondNumber;
+        console.log(seperator.repeat(25));
+        console.log("    Answer is : " + result);
+        console.log(seperator.repeat(25));
+        break;
+    //Multiple
+    case "Multiple":
+        result = answer.firstNumber * answer.secondNumber;
+        console.log(seperator.repeat(25));
+        console.log("    Answer is : " + result);
+        console.log(seperator.repeat(25));
+        break;
+    //Divide
+    case "Divide":
+        result = answer.firstNumber / answer.secondNumber;
+        console.log(seperator.repeat(25));
+        console.log("    Answer is : " + result);
+        console.log(seperator.repeat(25));
+        break;
+}
