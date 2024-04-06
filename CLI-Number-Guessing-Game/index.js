dcce9290eb06beb4ef4a5316cbb6739686a2e2ec
Nauman Khalid <naumankhalid217@gmail.com>
2024-04-06 18:08:03 +0500

    CLI-Number-Guessing-Game

---
+++ CLI-Number-Guessing-Game/index.js
@@ -0,0 +1,18 @@
+import inquirer from 'inquirer';
+//Welcome Message
+console.log("\n\tWelcome To \'ICodeIt\' - CLI-Number-Guessing-Game\n");
+// 1) computer will generate a random number
+// 2) user input for guessing number
+// 3) compare user input with computer generated number and show result
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt({
+    name: "userGuessNumber",
+    type: "number",
+    message: "Please guess a number between 1-10: ",
+});
+if (answer.userGuessNumber === randomNumber) {
+    console.log("Congratualtions! you guessed right number.");
+}
+else {
+    console.log("You guessed wrong number.");
+}
